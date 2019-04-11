1e35ebe8c5fc523809ad6713bfc594e2f6d37a19
shaz85 <32538597+shahzadEmbedded@users.noreply.github.com>
2019-04-11 16:08:25 +0500

    comments

--- commands.c
+++ commands.c
@@ -1,14 +1,16 @@
 /**
+* HMS V1.0 upto to 4 sensors 
 * Mega> start of command every command sould start with these leeters
 * C1 to recipent mobile numbers SMS and calls e.g Mega>C1,+923347455997,+9233----,+9233...
-* C2 global alerts enable/disable	Mega>C2,1
+* C2 global alerts enable/disable	Mega>C2,1 if you want to close debug messages then command will be "Mega>C2,10"
 * C3 Call enable/disable and number of times SMS repeat Mega>C3,1,2
 * C4 Signal level Mega>C4
-* C5 indvidual sensor enable/disable Mega>C5,111,111,000
+* C5 indvidual sensor enable/disable Mega>C5,111,111,000	// currently eight number sensor used to turn On/OFF low battery alerts.
 * C6 Reboot the device
 * C7 Get the device balance
-* C8 get the status of the device 
+* C8 get the status of the device description of which door open/close 
 * C9 
+* C10 command return back the status of battery, Signal level and firmware build time "Mega>C10"
 
 * C99 return the Configuration setting store in the EEPROM 
 
